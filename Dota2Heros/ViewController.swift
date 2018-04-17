f97c38033ac54752dd983fbfccce65be43b2fc44
Punyugi2012 <58160216@go.buu.ac.th>
2018-04-17 19:50:12 +0700

    ดึงข้อมูลจากapi

--- Dota2Heros/ViewController.swift
+++ Dota2Heros/ViewController.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Heros:Decodable {
+    var localized_name: String
+    var img: String
+}
 
+class ViewController: UIViewController {
+    var heros: [Heros] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://api.opendota.com/api/heroStats")
+        URLSession.shared.dataTask(with: url!) { (data, reponse, error) in
+            if error == nil {
+                do {
+                    self.heros = try JSONDecoder().decode([Heros].self, from: data!)
+                }
+                catch {
+                    print("Parse Fail")
+                }
+                print(self.heros)
+            }
+            else {
+                print("Found Error")
+            }
+        }.resume()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
