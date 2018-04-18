3661969725e6387828864cb72d4fa1a0aa138e95
Punyugi2012 <58160216@go.buu.ac.th>
2018-04-18 17:24:00 +0700

    เพิ่มปุ่ม refresh และ ตรวจสอบ connection

--- Dota2Heros/ViewController.swift
+++ Dota2Heros/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-struct Heros:Decodable {
+struct Hero:Decodable {
     var localized_name: String
     var img: String
 }
@@ -26,7 +26,7 @@ extension UIImageView {
             DispatchQueue.main.async() {
                 self.image = image
             }
-            }.resume()
+        }.resume()
     }
     func downloadedFrom(link: String, contentMode mode: UIViewContentMode = .scaleAspectFit) {
         guard let url = URL(string: link) else { return }
@@ -38,54 +38,68 @@ class ViewController: UIViewController, UICollectionViewDataSource {
 
     @IBOutlet weak var myCollectionView: UICollectionView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    var refreshController: UIRefreshControl!
     
-    var heros: [Heros] = []
+    var heros: [Hero] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         feedData()
-        self.refreshController = UIRefreshControl()
-        self.refreshController.addTarget(self, action: #selector(feedData), for: .valueChanged)
-        self.myCollectionView.addSubview(self.refreshController)
     }
+    
     @objc func feedData() {
-        let url = URL(string: "https://api.opendota.com/api/heroStats")
-        URLSession.shared.dataTask(with: url!) { (data, reponse, error) in
-            if error == nil {
-                do {
-                    self.heros = try JSONDecoder().decode([Heros].self, from: data!)
+        if Reachability.isConnectNetwork() {
+            let url = URL(string: "https://api.opendota.com/api/heroStats")
+            URLSession.shared.dataTask(with: url!) { (data, reponse, error) in
+                if error == nil {
+                    do {
+                        self.heros = try JSONDecoder().decode([Hero].self, from: data!)
+                    }
+                    catch {
+                        print("Parse Fail")
+                    }
+                    DispatchQueue.main.async {
+                        self.myCollectionView.reloadData()
+                        self.activityIndicator.stopAnimating()
+                    }
                 }
-                catch {
-                    print("Parse Fail")
+                else {
+                    print("Found Error")
                 }
-                DispatchQueue.main.async {
-                    self.myCollectionView.reloadData()
-                    self.activityIndicator.stopAnimating()
-                    self.refreshController.endRefreshing()
-                }
-            }
-            else {
-                print("Found Error")
-            }
             }.resume()
+        }
+        else {
+            DispatchQueue.main.async {
+                let alert = UIAlertController(title: nil, message: "Connection Network Fail", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: {
+                    self.activityIndicator.stopAnimating()
+                })
+            }
+        }
     }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return heros.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HeroCell", for: indexPath) as? HeroCollectionViewCell {
-            cell.heroName.text = heros[indexPath.row].localized_name
-            let defaultLink = "https://api.opendota.com"
+       let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HeroCell", for: indexPath) as! HeroCollectionViewCell
+        cell.heroName.text = heros[indexPath.row].localized_name
+        let defaultLink = "https://api.opendota.com"
+        if Reachability.isConnectNetwork() {
             cell.heroImage.downloadedFrom(link: defaultLink + heros[indexPath.row].img)
-            cell.heroImage.layer.cornerRadius = cell.heroImage.frame.height / 2
-            cell.heroImage.clipsToBounds = true
-            cell.heroImage.contentMode = .scaleAspectFill
-            return cell
         }
-        return UICollectionViewCell()
+        cell.heroImage.layer.cornerRadius = cell.heroImage.frame.height / 2
+        cell.heroImage.clipsToBounds = true
+        cell.heroImage.contentMode = .scaleAspectFill
+        return cell
     }
+    
+    @IBAction func refresh() {
+        self.activityIndicator.startAnimating()
+        feedData()
+    }
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
